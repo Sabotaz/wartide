5007996148985f0b6c0a42fcef130f8bd11ee6fd
Julien Christophe <sablier@zendikar.fr>
2026-02-13 22:00:35 +0100

    feat: add number of attacks

--- js/plundering.js
+++ js/plundering.js
@@ -1,6 +1,7 @@
 
 const troopLevel = document.querySelector("#troop_level");
 const fleetSize = document.querySelector("#fleet_size");
+const attacks = document.querySelector("#attacks");
 
 const troopLoadBonus = document.querySelector("#troop_load_bonus");
 const fleetLoadBonus = document.querySelector("#fleet_load_bonus");
@@ -14,6 +15,7 @@ const fScoutedWood = document.querySelector("#f_scouted_wood");
 const fScoutedIron = document.querySelector("#f_scouted_iron");
 const fScoutedCoal = document.querySelector("#f_scouted_coal");
 
+const totalPlunderedLoad = document.querySelector("#total_load");
 const plunderedWood = document.querySelector("#plundered_wood");
 const plunderedIron = document.querySelector("#plundered_iron");
 const plunderedCoal = document.querySelector("#plundered_coal");
@@ -22,6 +24,7 @@ const loadMeter = document.querySelector("#load_meter");
 
 troopLevel.addEventListener("input", calculate);
 fleetSize.addEventListener("input", calculate);
+attacks.addEventListener("input", calculate);
 troopLoadBonus.addEventListener("input", calculate);
 fleetLoadBonus.addEventListener("input", calculate);
 guildLoadBonus.addEventListener("input", calculate);
@@ -70,9 +73,6 @@ function calculate(event) {
     fScoutedIron.value = nFormatter(scoutedIron.value, 1)
     fScoutedCoal.value = nFormatter(scoutedCoal.value, 1)
 
-    loadBonus = 1 + (troopLoadBonus.value/100 + fleetLoadBonus.value/100 + guildLoadBonus.value/100) * loadBonusMultiplicator
-    totalLoad = fleetSize.value * armyLoads[troopLevel.value] * loadBonus
-
     scoutedWoodWeight = scoutedWood.value * 2
     scoutedIronWeight = scoutedIron.value * 2
     scoutedCoalWeight = scoutedCoal.value * 5
@@ -82,33 +82,76 @@ function calculate(event) {
     scoutedIronProp = scoutedIronWeight/totalScoutedWeight
     scoutedCoalProp = scoutedCoalWeight/totalScoutedWeight
 
-    maxPlunderedWood = totalLoad * scoutedWoodProp
-    maxPlunderedIron = totalLoad * scoutedIronProp
-    maxPlunderedCoal = totalLoad * scoutedCoalProp
+    loadBonus = 1 + (troopLoadBonus.value/100 + fleetLoadBonus.value/100 + guildLoadBonus.value/100) * loadBonusMultiplicator
 
-    minPlunderedWood = Math.min(maxPlunderedWood, scoutedWoodWeight)
-    minPlunderedIron = Math.min(maxPlunderedIron, scoutedIronWeight)
-    minPlunderedCoal = Math.min(maxPlunderedCoal, scoutedCoalWeight)
+    totalLoad = 0
+    maxLoad = 0
+    totalPlunderedWood = 0
+    totalPlunderedIron = 0
+    totalPlunderedCoal = 0
 
-    if (minPlunderedWood >= 2000) {
-        plunderedWood.value = nFormatter(minPlunderedWood / 2, 1) + " (" + Math.round(minPlunderedWood/2) + ")"
-    } else {
-        plunderedWood.value = Math.round(minPlunderedWood/2)
-    }
-    if (minPlunderedIron >= 2000) {
-        plunderedIron.value = nFormatter(minPlunderedIron / 2, 1) + " (" + Math.round(minPlunderedIron/2) + ")"
-    } else {
-        plunderedIron.value = Math.round(minPlunderedIron/2)
-    }
-    if (minPlunderedCoal >= 5000) {
-        plunderedCoal.value = nFormatter(minPlunderedCoal / 5, 1) + " (" + Math.round(minPlunderedCoal/5) + ")"
-    } else {
-        plunderedCoal.value = Math.round(minPlunderedCoal/5)
+    for (let i = 0; i < attacks.value; i++) {
+        if (totalLoad < 40000000) {
+            attackLoad = fleetSize.value * armyLoads[troopLevel.value] * loadBonus
+        } else {
+            attackLoad = fleetSize.value * armyLoads[troopLevel.value] * loadBonus * 0.15
+        }
+        maxLoad += attackLoad
+
+        remaining = 0
+
+        maxPlunderedWood = attackLoad * scoutedWoodProp
+        minPlunderedWood = Math.floor(Math.min(maxPlunderedWood, scoutedWoodWeight)/2)*2
+
+        maxPlunderedIron = attackLoad * scoutedIronProp
+        minPlunderedIron = Math.floor(Math.min(maxPlunderedIron, scoutedIronWeight)/2)*2
+
+        maxPlunderedCoal = attackLoad * scoutedCoalProp
+        minPlunderedCoal = Math.floor(Math.min(maxPlunderedCoal, scoutedCoalWeight)/5)*5
+
+        currentLoad = (minPlunderedWood + minPlunderedIron + minPlunderedCoal)
+
+        remaining = attackLoad - currentLoad
+
+        if (remaining >= 5) {
+            if (minPlunderedCoal < scoutedCoalWeight) {
+                minPlunderedCoal += 1
+                remaining -= 5
+            }
+        }
+
+        totalPlunderedWood += minPlunderedWood
+        totalPlunderedIron += minPlunderedIron
+        totalPlunderedCoal += minPlunderedCoal
+
+        scoutedWoodWeight -= minPlunderedWood
+        scoutedIronWeight -= minPlunderedIron
+        scoutedCoalWeight -= minPlunderedCoal
+
+        currentLoad = (minPlunderedWood + minPlunderedIron + minPlunderedCoal)
+        totalLoad += currentLoad
+
     }
 
-    currentLoad = (minPlunderedWood + minPlunderedIron + minPlunderedCoal)
-    load.value = Math.round(100*currentLoad/totalLoad) + "%"
-    loadMeter.value = Math.round(100*currentLoad/totalLoad)
+    if (totalPlunderedWood >= 2000) {
+        plunderedWood.value = nFormatter(totalPlunderedWood / 2, 1) + " (" + Math.round(totalPlunderedWood/2) + ")"
+    } else {
+        plunderedWood.value = Math.round(totalPlunderedWood/2)
+    }
+    if (totalPlunderedIron >= 2000) {
+        plunderedIron.value = nFormatter(totalPlunderedIron / 2, 1) + " (" + Math.round(totalPlunderedIron/2) + ")"
+    } else {
+        plunderedIron.value = Math.round(totalPlunderedIron/2)
+    }
+    if (totalPlunderedCoal >= 5000) {
+        plunderedCoal.value = nFormatter(totalPlunderedCoal / 5, 1) + " (" + Math.round(totalPlunderedCoal/5) + ")"
+    } else {
+        plunderedCoal.value = Math.round(totalPlunderedCoal/5)
+    }
+
+    load.value = Math.round(100*totalLoad/maxLoad) + "%"
+    loadMeter.value = Math.round(100*totalLoad/maxLoad)
+    totalPlunderedLoad.value = Math.round(totalLoad)
 }
 
 calculate({})
